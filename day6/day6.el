16692b4291921cc7c1e4fd71815df8404cc4ff39
Matti Uusitalo <uusitalo.matti@gmail.com>
2022-12-06 07:39:56 +0200

    Reuse find marker function

--- day6/day6.el
+++ day6/day6.el
@@ -7,36 +7,40 @@
 (defvar sample-data
   "mjqjpqmgbljsphdztnvjfqwrcgsmlb")
 
-(defun find-marker (s)
-  "Find start of packet marker from S."
-  (let ((pos 4))
+(defun find-marker (s n)
+  "Find packet marker from S the count of N."
+  (let ((pos n))
     (while (and (< pos (seq-length s))
-                (< (seq-length (seq-uniq (seq-subseq s (- pos 4) pos))) 4))
+                (< (seq-length (seq-uniq (seq-subseq s (- pos n) pos))) n))
       (setq pos (+ 1 pos)))
     pos))
 
-(find-marker sample-data)
+(defun find-sop-marker (s)
+  "Find start of packet marker from S.
 
-(find-marker "bvwbjplbgvbhsrlpgdmjqwftvncz")
-(find-marker "nppdvjthqldpwncqszvftbrmjlhg")
-(find-marker "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
+It is 4 unique characters in a row."
+  (find-marker s 4))
 
-(find-marker "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
+(find-marker sample-data 4)
+
+(find-sop-marker "bvwbjplbgvbhsrlpgdmjqwftvncz")
+(find-sop-marker "nppdvjthqldpwncqszvftbrmjlhg")
+(find-sop-marker "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
+
+(find-sop-marker "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
 
 (with-current-buffer (find-file-noselect "./input")
-  (find-marker (buffer-substring-no-properties (point-min) (point-max))))
+  (find-sop-marker (buffer-substring-no-properties (point-min) (point-max))))
 
 ;; 1920
 
 ;; Part 2
 
 (defun find-som-marker (s)
-  "Find start of message marker from S."
-  (let ((pos 14))
-    (while (and (< pos (seq-length s))
-                (< (seq-length (seq-uniq (seq-subseq s (- pos 14) pos))) 14))
-      (setq pos (+ 1 pos)))
-    pos))
+  "Find start of message marker from S.
+
+It is 14 unique characters in a row."
+  (find-marker s 14))
 
 (find-som-marker "mjqjpqmgbljsphdztnvjfqwrcgsmlb")
 
