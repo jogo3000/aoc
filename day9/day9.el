a237306c1fbb8a1700acc005dc0ca8e9f5b18286
Matti Uusitalo <uusitalo.matti@gmail.com>
2022-12-09 21:36:08 +0200

    Part 2 seems to progress well with sample data

--- day9/day9.el
+++ day9/day9.el
@@ -109,6 +109,92 @@ R 2")
 
 ;; part 2
 
+(defun run-simulation-v2 (initial-state input)
+  "Given INITIAL-STATE, count positions the tail will visit with given INPUT."
+  (let ((final-state
+         (seq-reduce
+          (lambda (acc line)
+            (let ((moves (parse-line line)))
+              (seq-reduce
+               (lambda (acc move)
+                 (let-alist acc
+                   ;; move the head
+                   (pcase move
+                     ("R" (setcar .head (+ (car .head) 1)))
+                     ("L" (setcar .head (- (car .head) 1)))
+                     ("U" (setcdr .head (+ (cdr .head) 1)))
+                     ("D" (setcdr .head (- (cdr .head) 1))))
+
+                   ;; move the other knots
+                   (let* ((new-t1 (maybe-adjust-tail .head .t1))
+                          (new-t2 (maybe-adjust-tail new-t1 .t2))
+                          (new-t3 (maybe-adjust-tail new-t2 .t3))
+                          (new-t4 (maybe-adjust-tail new-t3 .t4))
+                          (new-t5 (maybe-adjust-tail new-t4 .t5))
+                          (new-t6 (maybe-adjust-tail new-t5 .t6))
+                          (new-t7 (maybe-adjust-tail new-t6 .t7))
+                          (new-t8 (maybe-adjust-tail new-t7 .t8))
+                          (new-t9 (maybe-adjust-tail new-t8 .t9)))
+
+                     (when (> (knot-distance new-t9 .t9) 1)
+                     (setf (alist-get 't1 acc) new-t1)
+                     (setf (alist-get 't2 acc) new-t2)
+                     (setf (alist-get 't3 acc) new-t3)
+                     (setf (alist-get 't4 acc) new-t4)
+                     (setf (alist-get 't5 acc) new-t5)
+                     (setf (alist-get 't6 acc) new-t6)
+                     (setf (alist-get 't7 acc) new-t7)
+                     (setf (alist-get 't8 acc) new-t8)
+                     (setf (alist-get 't9 acc) new-t9)
+
+                     ;; Store the new position
+                     (setf (alist-get 'positions acc) (cons new-t9 .positions))))
+                 acc)
+               moves
+               acc)))
+          (string-lines
+           (string-chop-newline input))
+          initial-state)))
+    (let-alist final-state
+      (seq-uniq .positions))))
+
+(seq-length
+   (run-simulation-v2 '((head . (0 . 0))
+                        (t1 . (0 . 0))
+                        (t2 . (0 . 0))
+                        (t3 . (0 . 0))
+                        (t4 . (0 . 0))
+                        (t5 . (0 . 0))
+                        (t6 . (0 . 0))
+                        (t7 . (0 . 0))
+                        (t8 . (0 . 0))
+                        (t9 . (0 . 0))
+                        (positions . nil))
+                      sample-input))
+
+(seq-length
+ (run-simulation-v2 '((head . (0 . 0))
+                      (t1 . (0 . 0))
+                      (t2 . (0 . 0))
+                      (t3 . (0 . 0))
+                      (t4 . (0 . 0))
+                      (t5 . (0 . 0))
+                      (t6 . (0 . 0))
+                      (t7 . (0 . 0))
+                      (t8 . (0 . 0))
+                      (t9 . (0 . 0))
+                      (positions . nil))
+                    "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"))
+
+;; 36 - correct!
+
 
 
 
