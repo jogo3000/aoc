ff9bc593afe7f0f7cefde224f3930bd3fea73e6d
Matti Uusitalo <uusitalo.matti@gmail.com>
2022-12-09 07:10:11 +0200

    Sample input and asnwer added

--- day9/day9.el
+++ day9/day9.el
@@ -2,5 +2,23 @@
 ;;; Commentary:
 ;;; Code:
 
+(defvar sample-input
+  "R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2")
 
-;;; day9 ends here
+;; ..##..
+;; ...##.
+;; .####.
+;; ....#.
+;; s###..
+
+;; So, there are 13 positions the tail visited at least once.
+
+
+;;; day9.el ends here
