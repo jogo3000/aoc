87c7d2d7ffd0d5f978d255d7fa5b25338a28ca61
Matti Uusitalo <uusitalo.matti@gmail.com>
2022-12-12 07:22:14 +0200

    Lay out strategy
    Sample moved to own file so I can use the buffer as a data structure

--- day12/day12.el
+++ day12/day12.el
@@ -1,12 +1,13 @@
 ;;; Summary --- day12.el
 ;;; Commentary:
+;;; Looks like it'll make a lot of sense using buffers as data structures.
+;;; I can get the character at point with =char-to-string= and characters next
+;;; to it by using arithmetic or =next-line=, =previous-line=, =backward-char=,
+;;; =forward-char=
+;;;
 ;;; Code:
 
-(defvar sample-input
-  "Sabqponm
-abcryxxl
-accszExk
-acctuvwj
-abdefghi")
+(with-current-buffer (find-file-noselect "./sample-input")
+  (char-to-string (char-after)))
 
 ;;; day12.el ends here
