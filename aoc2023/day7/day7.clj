1c0027d2075b3342f6a2ae16653ea75b25d625b2
Matti Uusitalo <matti.uusitalo@solita.fi>
2023-12-07 11:50:27 +0200

    Part 2 solved

--- aoc2023/day7/day7.clj
+++ aoc2023/day7/day7.clj
@@ -79,3 +79,65 @@ QQQJA 483")
 (count-winnings (str/trim (slurp "/home/uusitalo/git/aoc/aoc2023/day7/input.txt")))
 
 ;; 251121738
+
+;; part 2
+
+(def joker-card-strengths '[\A \K \Q \T \9 \8 \7 \6 \5 \4 \3 \2 \J])
+
+(defn eval-hand2 [hand]
+  (let [cards (->> hand
+                   frequencies)
+        Js (cards \J)
+        without-Js (dissoc cards \J)
+        best-bet (or (ffirst (reverse (sort-by second without-Js))) \J) ;; All Js
+        redistributed (->> (-> cards
+                               (dissoc \J)
+                               (update best-bet (fn [n] (+ (or n 0) (or Js 0)))))
+                           (sort-by second)
+                           reverse)]
+    [(cond
+       (= 5 (second (first redistributed))) :five-of-a-kind
+       (= 4 (second (first redistributed))) :four-of-a-kind
+       (and (= 3 (second (first redistributed)))
+            (= 2 (second (second redistributed)))) :full-house
+       (and (= 3 (second (first redistributed)))
+            (= 1 (second (second redistributed)))) :three-of-a-kind
+       (and (= 2 (second (first redistributed)))
+            (= 2 (second (second redistributed)))) :two-pair
+       (and (= 2 (second (first redistributed)))
+            (= 1 (second (second redistributed)))) :one-pair
+       :else :high) hand]))
+
+(defn compare-hands2 [hand1 hand2]
+  (let [[hand1-value hand1-cards]
+        (eval-hand2 hand1)
+        [hand2-value hand2-cards]
+        (eval-hand2 hand2)
+        type-comp (- (.indexOf strengths hand1-value)
+                     (.indexOf strengths hand2-value))]
+    (if-not (zero? type-comp)
+      type-comp
+      (loop [hand1-left hand1-cards
+             hand2-left hand2-cards]
+        (let [h1-high (first hand1-left)
+              h2-high (first hand2-left)
+              val-comp (- (.indexOf joker-card-strengths h1-high)
+                          (.indexOf joker-card-strengths h2-high))]
+          (if-not (zero? val-comp)
+            val-comp
+            (recur (rest hand1-left)
+                   (rest hand2-left))))))))
+
+(defn count-winnings2 [input]
+  (let [hand-bids (->> input str/split-lines (map #(str/split % #"\s+")))
+        sorted (reverse (sort-by first compare-hands2 hand-bids))]
+    (->> sorted
+         (map-indexed (fn [rank [cs bid]]
+                        (* (inc rank) (parse-long bid))))
+         (reduce +))))
+
+(count-winnings2 sample-input)
+
+(count-winnings2 (str/trim (slurp "/home/uusitalo/git/aoc/aoc2023/day7/input.txt")))
+
+;; 251421071
