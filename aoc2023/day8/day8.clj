2578f9052cc813a2e9ce19ebe0e6df2792c9ae85
Matti Uusitalo <matti.uusitalo@solita.fi>
2023-12-08 07:19:01 +0200

    Part 1 done

--- aoc2023/day8/day8.clj
+++ aoc2023/day8/day8.clj
@@ -1,1 +1,47 @@
-(ns day8)
+(ns day8
+  (:require [clojure.string :as str]))
+
+(def sample-input-1 "RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)
+")
+
+(defn parse-input [s]
+  (let [[turns _ & nodes] (str/split-lines s)]
+    [turns (->> nodes
+                (map #(str/split % #"(\s*=\s*\()|(,\s*)|\)"))
+                (map (juxt first rest))
+                (into {}))]))
+
+(defn find-exit [input]
+  (let [[turns nodes] (parse-input input)]
+    (loop [steps 0
+           [this-step & next-steps] (cycle turns)
+           here "AAA"]
+      (println here)
+      (if (= here "ZZZ")
+        steps
+        (recur (inc steps)
+               next-steps
+               (cond
+                 (= this-step \R) (second (get nodes here))
+                 (= this-step \L) (first (get nodes here))
+                 :else (throw (Exception. (str "Not supposed to go here!: " this-step)))))))))
+
+(find-exit sample-input-1)
+
+(find-exit "LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)
+")
+
+(->> (slurp "/home/uusitalo/git/aoc/aoc2023/day8/input.txt")
+     find-exit) ;; 22199
