a105460efe3b011457b0ca4ec4c6969c714f38ef
Matti Uusitalo <uusitalo.matti@gmail.com>
2023-12-09 07:28:29 +0200

    Day 9 part 1 done

---
+++ aoc2023/day9/day9.clj
@@ -0,0 +1,42 @@
+(ns day9
+  (:require [clojure.string :as str]))
+
+(def sample-input-1 "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45")
+
+;; 0 3 6 9 12 15
+;;   3 3 3  3  3
+;;     0 0  0  0
+
+(defn differences [values]
+  (->> values
+       (partition 2 1)
+       (map (fn [[a b]] (- b a)))))
+
+(defn triangulate [line]
+  (let [values
+        (->> (str/split line #"\s+")
+             (map parse-long))]
+    (->> values
+         (iterate differences)
+         (take-while #(not-every? zero? %)))))
+
+(defn predict [line]
+  (let [ts (reverse (triangulate line))]
+    (reduce (fn [acc n]
+              (+ acc (last n))) 0 ts)))
+
+(->> sample-input-1
+     str/split-lines
+     (map predict)
+     (reduce +))
+
+
+(->> (slurp "/home/jogo3000/git/aoc2022/aoc2023/day9/input.txt")
+     str/trim
+     str/split-lines
+     (map predict)
+     (reduce +))
+
+;; 1916822650
