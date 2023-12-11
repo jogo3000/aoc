3fafe9c9054c86b0e81c04a0c5fec5ac56ece573
Matti Uusitalo <matti.uusitalo@solita.fi>
2023-12-11 08:14:08 +0200

    Day 11 part 2 solved

--- aoc2023/day11/day11.clj
+++ aoc2023/day11/day11.clj
@@ -108,3 +108,42 @@
 (->> (slurp "/home/uusitalo/git/aoc/aoc2023/day11/input.txt")
      parse-input
      find-shortest-paths) ; 9947476
+
+
+;; Part deux
+;; 1 000 000
+
+(->> (slurp "/home/uusitalo/git/aoc/aoc2023/day11/input.txt")
+     parse-input
+     (find-voids)) ;; Lot of void, need to be smarter
+
+;; Because it is manhattan distance, no actual need to add the rows
+(defn find-shortest-paths-smart [universe]
+  (let [[col-voids row-voids] (find-voids universe)
+        height (count universe)
+        width (count (first universe))
+        galaxies
+        (for [y (range height)
+              x (range width)
+              :when (= \# (get-in universe [y x]))]
+          [y x])]
+    (reduce +
+            (loop [distances []
+                   [galaxy & other-galaxies] galaxies]
+              (if (empty? galaxy) distances
+                  (recur (into distances
+                               (for [[y2 x2] other-galaxies]
+                                 (let [[y x] galaxy
+                                       y-space (range (min y y2) (max y y2))
+                                       x-space (range (min x x2) (max x x2))
+                                       y-voids (filter col-voids y-space)
+                                       x-voids (filter row-voids x-space)]
+                                   (+ (abs (- y2 y))
+                                      (* 999999 (count y-voids))
+                                      (abs (- x2 x))
+                                      (* 999999 (count x-voids))))))
+                         other-galaxies))))))
+
+(->> (slurp "/home/uusitalo/git/aoc/aoc2023/day11/input.txt")
+     parse-input
+     find-shortest-paths-smart) ; 519939907614
