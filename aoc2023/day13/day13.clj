ee977455c4d9ef68b1709d49e2094ac23d68ced6
Matti Uusitalo <matti.uusitalo@solita.fi>
2023-12-13 15:45:34 +0200

    I don't get the puzzle logic.

--- aoc2023/day13/day13.clj
+++ aoc2023/day13/day13.clj
@@ -63,3 +63,37 @@
 ; 35210
 
 ;; Part deux
+;;
+;; Looks like maybe the smudges gonna be on the line not
+;; covered. Otherwise every change is only going to make things worse,
+;; eh?
+
+(defn uncovered-by-mirror [len n]
+  (if (= (/ len n) 2)
+    [] ;; Nothing uncovered
+    (let [d (- len n)]
+      (if (> n d)
+        [1 (if (= d (dec n)) 2 (- n d))]
+        [(* 2 n) len]))))
+
+(defn swap [[a b]]
+  [b a])
+
+;; Huh? the first reflecton is still there?
+(let [[hz vr] (parse-input sample1)
+      width (count vr)
+      height (count hz)]
+  [(appraise [hz vr])
+   (for [n (->> vr
+                find-mirror-pos
+                first
+                (uncovered-by-mirror width)
+                (apply range))]
+     (let [uncovered (get vr (dec n))]
+       (keep-indexed
+        (fn [x c]
+          (let [new-map
+                (str/join \newline
+                          (update vr (dec n)
+                                  (fn [line] (str/join (assoc (vec line) x (if (= \. c) \# \.))))))]
+            (appraise (swap (parse-input new-map))))) uncovered)))])
