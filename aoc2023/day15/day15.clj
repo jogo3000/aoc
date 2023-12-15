91bd5e2b78a76b37af04e55ac947521b62925b2a
Matti Uusitalo <matti.uusitalo@solita.fi>
2023-12-15 07:09:53 +0200

    Part 1 done

--- aoc2023/day15/day15.clj
+++ aoc2023/day15/day15.clj
@@ -1,2 +1,25 @@
 (ns day15
   (:require [clojure.string :as str]))
+
+(defn HASH [s]
+  (reduce (fn [acc c]
+            (rem (* (+ acc (int c)) 17)
+                 256))
+          0
+          s))
+
+(HASH "HASH")
+
+(def sample "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7")
+
+(->> (str/split sample #",")
+     (map str/trim)
+     (map HASH)
+     (reduce +))
+
+(->> (str/split (slurp "/home/uusitalo/git/aoc/aoc2023/day15/input.txt") #",")
+     (map str/trim)
+     (map HASH)
+     (reduce +))
+
+; 509152
