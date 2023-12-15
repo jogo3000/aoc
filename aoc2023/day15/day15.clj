07719cf0c97f672c829ea1329e126dca89d883f3
Matti Uusitalo <matti.uusitalo@solita.fi>
2023-12-15 07:59:34 +0200

    Right answer for sample data, wrong for whole input?

--- aoc2023/day15/day15.clj
+++ aoc2023/day15/day15.clj
@@ -23,3 +23,60 @@
      (reduce +))
 
 ; 509152
+
+;; Part deux
+
+(defn parse-label [s]
+  (if (str/ends-with? s "-") ;; Remove direction
+    [:remove [(subs s 0 (dec (count s)))]]
+    [:add (str/split s #"=")]))
+
+(defn add-to-box [box label]
+  (loop [pos 0]
+    (cond (>= pos (count box))
+          (conj box label)
+
+          (= (first (get box pos))
+             (first label))
+          (assoc box pos label)
+
+          :else (recur (inc pos)))))
+
+(defn remove-from-box [box label]
+  (loop [pos 0]
+    (cond (>= pos (count box))
+          box
+
+          (= (first (get box pos))
+             (first label))
+          (let [[head tail] (split-at (inc pos) box)]
+            (into (vec (rest head)) tail))
+
+          :else (recur (inc pos)))))
+
+(defn install-lenses [instructions]
+  (->> (str/split instructions #",")
+       (map str/trim)
+       (map parse-label)
+       (reduce (fn [acc [instr label]]
+                 (let [hashcode (HASH (first label))]
+                   (update acc hashcode
+                           (fn [box]
+                             (let [box (or box [])]
+                               (if (= instr :add)
+                                 (add-to-box box label)
+                                 (remove-from-box box label)))))))
+               {})))
+
+(defn focusing-power [input]
+  (->> (install-lenses input)
+       (mapcat (fn [[i box]]
+                 (map-indexed (fn [j [_ focal-length]]
+                                (* (inc i) ;; one plus box number
+                                   (inc j) ;; slot number of the lens
+                                   (parse-long focal-length))) box)))
+       (reduce +)))
+
+(focusing-power sample)
+(focusing-power (slurp "/home/uusitalo/git/aoc/aoc2023/day15/input.txt"))
+;; 231852
