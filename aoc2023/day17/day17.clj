44a9a04c6e95955b21ab9e3d1565f6d26f183216
Matti Uusitalo <uusitalo.matti@gmail.com>
2023-12-19 20:23:23 +0200

    Does part 1 correctly day 12

--- aoc2023/day17/day17.clj
+++ aoc2023/day17/day17.clj
@@ -34,10 +34,10 @@
          (<= 0 x (dec width)))))
 
 (def dirs {nil [south east]
-           west [west north south]
-           east [east north south]
-           north [north east west]
-           south [south east west]})
+           west [#_west north south]
+           east [#_east north south]
+           north [#_north east west]
+           south [#_south east west]})
 
 (defn safe+ [& args]
   (reduce (fn [acc n]
@@ -68,60 +68,73 @@
 (defn distance [[y1 x1] [y2 x2]]
   (+ (abs (- y2 y1)) (abs (- x2 x1))))
 
+(defn neighbours-in-dir [m current d']
+  (:acc
+   (reduce (fn [{:keys [acc new-heat
+                        pos] :as all} n]
+             (let [speed' (+ n
+                             (if (= d' (:dir current))
+                               (:speed current)
+                               0))]
+               (if (or (> speed' 3)
+                       (not (may-move? m pos d')))
+                 all
+                 (let [pos' (d' pos)
+                       heat' (+ new-heat (get-in m pos'))]
+                   {:acc (cons [pos' heat' speed' d'] acc)
+                    :new-heat heat'
+                    :pos pos'}))))
+           {:acc '()
+            :pos (:pos current)
+            :new-heat 0}
+           (range 1 4))))
+
 (defn astar [m start goal h]
   (let [height (count m)
         width (count (first m))
+        visited (atom #{})
+        f-score (atom {start (h start)})
+        g-score (atom {start 0})
         ^PriorityQueue open-set (PriorityQueue.
                                  (reify Comparator
                                    (compare ^int [_this o1 o2]
-                                     (- (:heat o1) (:heat o2)))))
-        f-score (atom {start (h start)})
-        g-score (atom {start 0})]
+                                     (- (+ (:heat o1)
+                                           (distance goal (:pos o1)))
+
+                                        #_(@f-score o1 Long/MAX_VALUE)
+                                        (+ (:heat o2)
+                                           (distance goal (:pos o2)))
+                                        #_(@f-score o2 Long/MAX_VALUE)))))]
     (.add open-set (->QueueElement 0 [0 0] '([0 0]) 0 nil))
     (loop []
-      (if (.isEmpty open-set) :failure
+      (if (.isEmpty open-set) @g-score
           (let [current (.remove open-set)]
             (if (= (:pos current) goal)
               current
               (do
-                (doseq [path
-                        (->> (dirs (:dir current))
-                             (map (fn [d']
-                                       (:acc
-                                        (reduce (fn [{:keys [acc new-heat
-                                                             pos] :as all} n]
-                                                  (let [speed' (+ n
-                                                                  (if (= d' (:dir current))
-                                                                    (:speed current)
-                                                                    0))]
-                                                    (if (or (> speed' 3)
-                                                            (not (may-move? m pos d')))
-                                                      all
-                                                      (let [pos' (d' pos)
-                                                            heat' (+ new-heat (get-in m pos'))]
-                                                        {:acc (cons [pos' heat' speed' d'] acc)
-                                                         :new-heat heat'
-                                                         :pos pos'}))))
-                                                {:acc '()
-                                                 :pos (:pos current)
-                                                 :new-heat 0}
-                                                (range 1 4))))))]
-                  (when (seq path)
-                    (let [[n-pos n-heat n-speed n-dir] (first path)
-                          tentative-g-score (safe+ (@g-score (:pos current) Long/MAX_VALUE)
-                                                   n-heat)]
-                      (when (< tentative-g-score (@g-score n-pos Long/MAX_VALUE))
-                        (swap! g-score assoc n-pos tentative-g-score)
-                        (swap! f-score assoc n-pos (safe+ tentative-g-score (h n-pos)))
+                (when-not (contains? @visited [(:pos current)
+                                               (:dir current)])
+                  (swap! visited conj [(:pos current) (:dir current)])
+                  (doseq [[n-pos n-heat n-speed n-dir]
+                          (->> (dirs (:dir current))
+                               (mapcat (partial neighbours-in-dir m current))
+                               reverse)]
+                    (let [#_#_tentative-g-score (safe+ (:heat current) n-heat)]
+                      (when true #_(< tentative-g-score (@g-score n-pos Long/MAX_VALUE))
+                            #_(swap! g-score assoc n-pos tentative-g-score)
+                            #_(swap! f-score assoc n-pos (safe+ tentative-g-score (h n-pos)))
                         (.add open-set (->QueueElement
                                         (+ (:heat current) n-heat)
                                         n-pos
-                                        (into (map first path) (:visited current))
+                                        (cons n-pos (:visited current))
                                         n-speed
                                         n-dir))))))
                 (recur))))))))
 
-(astar (parse-input sample-input) [0 0] [12 12] (fn [a] (distance [12 12] a)))
+
+((astar (parse-input sample-input) [0 0] [12 12] (fn [a] (distance [12 12] a))) [12 12])
+
+(def res (astar (parse-input sample-input) [0 0] [12 12] (fn [a] (distance [12 12] a))))
 
 (defn visualize [m steps dirs]
   (let [height (count m)
@@ -141,7 +154,13 @@
                 :else \#))
             (get-in m [y x]))))))))
 
-#_(let [m (parse-input sample-input)
-      p (set (second (result [12 12])))]
+
+(let [m (parse-input sample-input)
+      p (set (:visited res))]
   (println "---------")
   (println (visualize m p {})))
+
+(let [m (parse-input (slurp "day17/input.txt"))
+      max-y (dec (count m))
+      max-x (dec (count (first m)))]
+  (:heat (astar (parse-input (slurp "day17/input.txt")) [0 0] [max-y max-x] (fn [a] (distance [12 12] a)))))
