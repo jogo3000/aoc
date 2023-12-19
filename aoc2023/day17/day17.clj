b33ab0419046dc47fa6074466ebad1bf162eedd9
Matti Uusitalo <uusitalo.matti@gmail.com>
2023-12-19 19:04:02 +0200

    Trying to reimplement a*

--- aoc2023/day17/day17.clj
+++ aoc2023/day17/day17.clj
@@ -57,23 +57,6 @@
       (> y y') north
       :else nil)))
 
-(defn reconstruct-path [came-from current]
-  (loop [total-path (list current)
-         current current]
-    (if-let [c' (came-from current)]
-      (recur (cons c' total-path)
-             c')
-      total-path)))
-
-(defn follow-path-back [prev u]
-  (->> (reconstruct-path prev u)
-       (reverse)
-       (take 4)
-       (remove nil?)
-       (partition 2 1)
-       (map #(direction (first %)
-                        (second %)))))
-
 (defn evaluate-path [m p]
   (->> p
        butlast
@@ -82,65 +65,63 @@
 
 (defrecord QueueElement [heat pos visited speed dir])
 
-(defn djikstra [m]
-  (let [source [0 0]
-        max-y (dec (count m))
-        max-x (dec (count (first m)))
-        target [max-y max-x]
-        Q (PriorityQueue. (reify
-                            Comparator
-                            (compare ^int [this o1 o2]
-                              (let [[y1 x1] (:pos o1)
-                                    [y2 x2] (:pos o2)]
-                                (- (+ (:heat o1)
-                                      (+ (- max-y y1)
-                                         (- max-x x1)))
-                                   (+ (:heat o2)
-                                      (+ (- max-y y2)
-                                         (- max-x x2))))))))]
-    (.add Q (->QueueElement 0 source #{source} 0 nil))
+(defn distance [[y1 x1] [y2 x2]]
+  (+ (abs (- y2 y1)) (abs (- x2 x1))))
+
+(defn astar [m start goal h]
+  (let [height (count m)
+        width (count (first m))
+        ^PriorityQueue open-set (PriorityQueue.
+                                 (reify Comparator
+                                   (compare ^int [_this o1 o2]
+                                     (- (:heat o1) (:heat o2)))))
+        f-score (atom {start (h start)})
+        g-score (atom {start 0})]
+    (.add open-set (->QueueElement 0 [0 0] '([0 0]) 0 nil))
     (loop []
-      (let [u (.remove Q)]
-        (if (= (:pos u) target)
-          u
-          (let [heat-loss (:heat u)
-                pos (:pos u)
-                speed-limit (>= (:speed u) 3)
-                dir (:dir u)
-                neighbours (into []
-                                 (comp
-                                  (filter #(if speed-limit
-                                             (not= % dir)
-                                             %))
-                                  (filter #(may-move? m pos %))
-                                  (map #(% pos))
-                                  (remove (:visited u)))
-                                 (dirs dir))]
-            (doseq [v neighbours]
-              (let [dir (direction pos v)]
-                (.offer Q (->QueueElement (+ heat-loss (get-in m v))
-                                          v
-                                          (conj (:visited u) v)
-                                          (if (not= dir (:dir u))
-                                            1
-                                            (inc (:speed u)))
-                                          dir))))
-            (recur)))))))
+      (if (.isEmpty open-set) :failure
+          (let [current (.remove open-set)]
+            (if (= (:pos current) goal)
+              current
+              (do
+                (doseq [path
+                        (->> (dirs (:dir current))
+                             (map (fn [d']
+                                       (:acc
+                                        (reduce (fn [{:keys [acc new-heat
+                                                             pos] :as all} n]
+                                                  (let [speed' (+ n
+                                                                  (if (= d' (:dir current))
+                                                                    (:speed current)
+                                                                    0))]
+                                                    (if (or (> speed' 3)
+                                                            (not (may-move? m pos d')))
+                                                      all
+                                                      (let [pos' (d' pos)
+                                                            heat' (+ new-heat (get-in m pos'))]
+                                                        {:acc (cons [pos' heat' speed' d'] acc)
+                                                         :new-heat heat'
+                                                         :pos pos'}))))
+                                                {:acc '()
+                                                 :pos (:pos current)
+                                                 :new-heat 0}
+                                                (range 1 4))))))]
+                  (when (seq path)
+                    (let [[n-pos n-heat n-speed n-dir] (first path)
+                          tentative-g-score (safe+ (@g-score (:pos current) Long/MAX_VALUE)
+                                                   n-heat)]
+                      (when (< tentative-g-score (@g-score n-pos Long/MAX_VALUE))
+                        (swap! g-score assoc n-pos tentative-g-score)
+                        (swap! f-score assoc n-pos (safe+ tentative-g-score (h n-pos)))
+                        (.add open-set (->QueueElement
+                                        (+ (:heat current) n-heat)
+                                        n-pos
+                                        (into (map first path) (:visited current))
+                                        n-speed
+                                        n-dir))))))
+                (recur))))))))
 
-;; I think this needs a more efficient priority queue
-(def result (djikstra (parse-input sample-input)))
-
-#_(result [12 12])
-
-#_(reconstruct-path result [12 12])
-
-#_(search-path  (parse-input puzzle-input))
-
-#_(let [m (parse-input sample-input)]
-  (reduce (fn [acc pos]
-            (+ acc (get-in m pos)))
-          0
-          (reconstruct-path result [12 12])))
+(astar (parse-input sample-input) [0 0] [12 12] (fn [a] (distance [12 12] a)))
 
 (defn visualize [m steps dirs]
   (let [height (count m)
@@ -158,7 +139,7 @@
                 (= north d) \^
                 (= south d) \v
                 :else \#))
-           (get-in m [y x]))))))))
+            (get-in m [y x]))))))))
 
 #_(let [m (parse-input sample-input)
       p (set (second (result [12 12])))]
