95012ce849e0eca47cfb984244c4e64f1b830f3c
Matti Uusitalo <uusitalo.matti@gmail.com>
2023-12-30 13:17:36 +0200

    Solved part 1

--- aoc2015/day8/day8.clj
+++ aoc2015/day8/day8.clj
@@ -5,17 +5,36 @@
 (def puzzle-input (slurp "day8/input.txt"))
 
 
-(let [s (slurp "day8/input.txt")]
-                                        ;(-
-  #_(count (str/replace s #"\n" ""))
-  (->>
-   (edn/read-string
-    (str "["
-         (str/replace s #"\\x" "\\\\u00")
-         "]"))
-   #_#_(map count)
-   (reduce +))
+(let [s puzzle-input]
+  (-
+   (count (str/replace s #"\s+" ""))
+   (->>
+    (edn/read-string
+     (str "["
+          (str/replace s #"\Q\x\E" "\\\\u00")
+          "]"))
+    (map count)
+    (reduce +))
 
-                                        ;   )
+   )
 
   ) ; 1326 too low
+
+(defn decode [s]
+  (-> s
+      (str/replace #"\Q\\\E" "_")
+      (str/replace #"\\x\p{XDigit}{2}" "_")
+      (str/replace #"\\\"" "_")
+      (str/replace #"\"" "")))
+
+(let [s (str/trim puzzle-input)
+      parts (map str/trim (str/split-lines s))]
+  (- (->> parts
+          (map count)
+          (reduce +))
+     (->> parts
+          (map (comp count decode))
+          (reduce +)))) 1342
+
+
+;; Part deux
