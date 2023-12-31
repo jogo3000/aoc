61e6a4d85046851f89d555109f0395c946b84a01
Matti Uusitalo <uusitalo.matti@gmail.com>
2023-12-31 07:50:14 +0200

    Parsed the puzzle input

--- aoc2015/day9/day9.clj
+++ aoc2015/day9/day9.clj
@@ -1,6 +1,32 @@
-(ns day9)
+(ns day9
+  (:require [clojure.string :as str]))
 
 ;; Traveling salesman problem with 28 cities, brute force is not going to fly.
+;; Well actually, there aren't edges between all towns. Perhaps it can be brute
+;; forced after all.
+(def puzzle-input (slurp "day9/input.txt"))
 
-(let [n 28]
-  (* (Math/pow n 2) (Math/pow 2 n)))
+(defn parse-line [s]
+  (let [[a b c] (str/split s #"( to )|( = )")]
+    [[a b] c]))
+
+(defn parse-input [s]
+  (->> s
+       str/trim
+       str/split-lines
+       (map parse-line)
+       (into {})))
+
+(def distance-matrix (parse-input puzzle-input))
+
+(def locations (->> distance-matrix keys flatten set))
+
+(count locations) ;; 8 So it's actually only 8 cities
+
+;; Possible moves would be list of distances * 2
+
+(def possible-moves (into (keys distance-matrix)
+                          (->> distance-matrix keys reverse)))
+
+(count possible-moves); 56, but this list already contains possibilities of
+                      ; going through cities twice!
