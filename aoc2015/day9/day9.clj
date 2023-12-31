394121d87caeafa25942776ac98905e8815c9afc
Matti Uusitalo <uusitalo.matti@gmail.com>
2023-12-31 08:41:43 +0200

    Both parts done. It wasn't so bad?

--- aoc2015/day9/day9.clj
+++ aoc2015/day9/day9.clj
@@ -8,7 +8,7 @@
 
 (defn parse-line [s]
   (let [[a b c] (str/split s #"( to )|( = )")]
-    [[a b] c]))
+    [[a b] (parse-long c)]))
 
 (defn parse-input [s]
   (->> s
@@ -18,6 +18,9 @@
        (into {})))
 
 (def distance-matrix (parse-input puzzle-input))
+(def distances-both-directions (into distance-matrix
+                                     (map (fn [[[a b] d]]
+                                            [[b a] d]) distance-matrix)))
 
 (def locations (->> distance-matrix keys flatten set))
 
@@ -26,7 +29,58 @@
 ;; Possible moves would be list of distances * 2
 
 (def possible-moves (into (keys distance-matrix)
-                          (->> distance-matrix keys reverse)))
+                          (->> distance-matrix keys (map (comp vec reverse)))))
 
 (count possible-moves); 56, but this list already contains possibilities of
                       ; going through cities twice!
+
+(defn travelling-santa [travelling-santa visited path distance]
+  (if (= (set visited) locations)
+    distance
+    (let [here (first path)
+          possible-moves
+          (->> possible-moves
+               (filter (fn [[from to]]
+                         (when-not (contains? visited to)
+                           (= from here)))))]
+      (reduce (fn [acc [from to]]
+                (min acc (travelling-santa
+                          travelling-santa
+                          (conj visited to)
+                          (cons to path)
+                          (+ distance (get distances-both-directions [from to])))))
+              Long/MAX_VALUE
+              possible-moves))))
+
+(def mem-travelling-santa (memoize travelling-santa))
+
+(for [loc locations]
+  (mem-travelling-santa mem-travelling-santa #{loc} (list loc) 0))
+
+;; Shortest route is 207
+
+;; Part deux is longest route
+(defn scenic-route-travelling-santa [travelling-santa visited path distance]
+  (if (= (set visited) locations)
+    distance
+    (let [here (first path)
+          possible-moves
+          (->> possible-moves
+               (filter (fn [[from to]]
+                         (when-not (contains? visited to)
+                           (= from here)))))]
+      (reduce (fn [acc [from to]]
+                (max acc (travelling-santa
+                          travelling-santa
+                          (conj visited to)
+                          (cons to path)
+                          (+ distance (get distances-both-directions [from to])))))
+              Long/MIN_VALUE
+              possible-moves))))
+
+(def mem-scenic (memoize scenic-route-travelling-santa))
+
+(for [loc locations]
+  (mem-scenic mem-scenic #{loc} (list loc) 0))
+
+;; Longest is 804
