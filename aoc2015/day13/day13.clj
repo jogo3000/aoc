b4d542ea3ad1b2ed9a0183174d581bb8622ad800
Matti Uusitalo <uusitalo.matti@gmail.com>
2024-01-05 20:01:45 +0200

    Parsing done

--- aoc2015/day13/day13.clj
+++ aoc2015/day13/day13.clj
@@ -18,3 +18,18 @@ David would gain 41 happiness units by sitting next to Carol.
 ")
 
 ;; Optimal total change of happiness for sample is 330
+
+(defn parse-line [s]
+  (let [[_ person-a gain-or-lose units person-b]
+        (re-find #"(\w+) would (\w+) (\d+) happiness units by sitting next to (\w+)\." s)]
+    [person-a person-b (case gain-or-lose
+                "gain" (parse-long units)
+                "lose" (- (parse-long units)))]))
+
+(defn parse-happiness-key [input]
+  (->> (str/split-lines input)
+       (map parse-line)
+       (reduce (fn [acc [person-a person-b units]]
+                 (assoc-in acc [person-a person-b] units))
+               {})))
+
