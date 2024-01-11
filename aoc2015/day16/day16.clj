84ee3925a444da21c81d4f13d1b7c91613986dc6
Matti Uusitalo <uusitalo.matti@gmail.com>
2024-01-11 17:51:55 +0200

    Part 2 solved

--- aoc2015/day16/day16.clj
+++ aoc2015/day16/day16.clj
@@ -31,3 +31,19 @@ perfumes: 1
                    sue-no)))))) ;; ("Sue 373")
 
 ;; part 2
+(let [mfcsam (parse-analysis MFCSAM-analysis)]
+  (->> puzzle-input
+       str/trim
+       str/split-lines
+       (keep (fn [s]
+               (let [[sue-no memo] (str/split s #":" 2)
+                     parsed-memo (parse-analysis memo)]
+                 (when (every? (fn [[k v]]
+                                 (condp = k
+                                   'cats (> v (get mfcsam k))
+                                   'pomeranians (< v (get mfcsam k))
+                                   'goldfish (< v (get mfcsam k))
+                                   (= v (get mfcsam k)))) parsed-memo)
+                   sue-no)))))) ;; ("Sue 260")
+
+;; That was easy?
