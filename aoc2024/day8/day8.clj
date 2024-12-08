ae915cc07b05c532341dfdf01fe069268347cb2e
Matti Uusitalo <uusitalo.matti@gmail.com>
2024-12-08 07:52:18 +0200

    Solved part 1

--- aoc2024/day8/day8.clj
+++ aoc2024/day8/day8.clj
@@ -18,8 +18,11 @@
 
 (def puzzle-input (slurp "day8/input"))
 
+(defn into-map [input]
+  (->> input str/split-lines (map vec) vec))
+
 (defn find-antennas [input]
-  (let [m (->> input str/split-lines (map vec) vec)
+  (let [m (into-map input)
         height (count m)
         width (count (first m))]
     (for [x (range height)
@@ -27,7 +30,6 @@
           :let [tile (get-in m [x y])]
           :when (not= tile \.)]
       [tile [x y]])))
-
 
 (defn group-antenna-locations [antennas]
   (update-vals
@@ -50,9 +52,14 @@
         yd (- y2 y1)]
     [(+ x2 xd) (+ y2 yd)]))
 
+(defn within-map? [m antinode]
+  (let [max-x (count m)
+        max-y (count (first m))
+        [a b] antinode]
+    (and (>= (dec max-x) a 0) (>= (dec max-y) b 0))))
+
 (defn find-all-antinodes [input]
-  (let [max-x (count input)
-        max-y (count (first (str/split-lines input)))]
+  (let [m (into-map input)]
     (->> (update-vals
           (->> input
                find-antennas
@@ -61,10 +68,23 @@
          vals
          (apply concat)
          (map find-antinode)
-         (filter (fn [[a b]] (and (>= (dec max-x) a 0) (>= (dec max-y) b 0))))
-         (into #{})
-         count)))
+         (filter (partial within-map? m))
+         (into #{}))))
 
-(find-all-antinodes sample-input) ; 14
 
-(find-all-antinodes puzzle-input) ; 401
+(let [input puzzle-input
+      m (into-map input)]
+  (println
+   (str/join "\n"
+             (map str/join
+                  (reduce (fn [acc [x y]]
+                            (if (within-map? m [x y])
+                              (assoc-in acc [x y] \#)
+                              acc))
+                          m
+                          (find-all-antinodes input))))))
+
+
+(count (find-all-antinodes sample-input)) ; 14
+
+(count (find-all-antinodes puzzle-input)) ; 344
