173633bb2016000f31f1daa09bfc41a46c89c213
Matti Uusitalo <uusitalo.matti@gmail.com>
2024-12-10 19:43:31 +0200

    This was real easy so I solved part 2 too

--- aoc2024/day10/day10.clj
+++ aoc2024/day10/day10.clj
@@ -47,28 +47,47 @@
            :when (zero? tile)]
        [[x y]]))))
 
-(let [m (parse-input larger-sample)
-      max-x (count m)
-      max-y (count (first m))
-      starting-positions
-      (find-starting-positions m)]
-  (->>
-   (loop [cursors starting-positions]
-     (if (every? #(= 9 (get-in m (peek %))) cursors) cursors
-         (recur
-          (->> cursors
-               (reduce (fn [acc cursor]
-                         (let [pos (peek cursor)
-                               tile (get-in m pos)]
-                           (if (= tile 9) (into acc cursor)
-                               (into acc
-                                     (comp
-                                      (filter #(= (get-in m %) (inc tile)))
-                                      (map #(conj cursor %)))
-                                     [(north pos)
-                                      (south pos)
-                                      (east pos)
-                                      (west pos)])))) [])))))
-   (map (juxt first peek))
-   (into #{})
-   count))
+
+(defn find-trails [input]
+  (let [m (parse-input input)
+        starting-positions
+        (find-starting-positions m)]
+    (->>
+     (loop [cursors starting-positions]
+       (if (every? #(= 9 (get-in m (peek %))) cursors) cursors
+           (recur
+            (->> cursors
+                 (reduce (fn [acc cursor]
+                           (let [pos (peek cursor)
+                                 tile (get-in m pos)]
+                             (if (= tile 9) (into acc cursor)
+                                 (into acc
+                                       (comp
+                                        (filter #(= (get-in m %) (inc tile)))
+                                        (map #(conj cursor %)))
+                                       [(north pos)
+                                        (south pos)
+                                        (east pos)
+                                        (west pos)])))) []))))))))
+
+(defn evaluate-paths-pt1 [paths]
+  (->> paths (map (juxt first peek)) (into #{}) count))
+
+(->> larger-sample
+     find-trails
+     evaluate-paths-pt1)
+
+(->> (slurp "day10/input")
+     find-trails
+     evaluate-paths-pt1) ; 667
+
+(defn evaluate-paths-pt2 [paths]
+  (->> paths distinct count))
+
+(->> larger-sample
+     find-trails
+     evaluate-paths-pt2)
+
+(->> (slurp "day10/input")
+     find-trails
+     evaluate-paths-pt2) ; 1344
