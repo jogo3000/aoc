3036bec573250fb4151d84a83554d43d46e863b0
Matti Uusitalo <uusitalo.matti@gmail.com>
2025-12-07 06:46:15 +0200

    Solved part 2

--- aoc2025/day3/day3.clj
+++ aoc2025/day3/day3.clj
@@ -23,3 +23,28 @@
      str/split-lines
      (map optimal-power)
      (reduce +)) ; 17142
+
+;; part 2
+
+(defn optimal-power+ [s n]
+  (if (zero? n) 0
+      (let [len (count s)
+            [pos best-first]
+            (->> s
+                 (take (- len (dec n)))
+                 (map-indexed (fn [i c]
+                                [i (-> c str parse-long)]))
+                 (reduce (fn [[i x] [j n]]
+                           (if (> n x) [j n] [i x]))))]
+        (+ (* (java.lang.Math/pow 10 (dec n)) best-first)
+           (optimal-power+ (subs s (inc pos)) (dec n))))))
+
+(->> sample
+     str/split-lines
+     (map (fn [s] (optimal-power+ s 12)))
+     (reduce +)) ; 3.121910778619E12
+
+(->> (slurp "day3/input")
+     str/split-lines
+     (map (fn [s] (optimal-power+ s 12)))
+     (reduce +)) ; 1.69935154100102E14
