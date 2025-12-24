eafb96a3804151f9f5a7a7a3723d38180d4eb030
Matti Uusitalo <mattiuusitalo@MacBookPro.wlan>
2025-12-24 09:12:13 +0200

    Rotate and reflect

--- aoc2025/day12/day12.clj
+++ aoc2025/day12/day12.clj
@@ -6,7 +6,12 @@
 (defn parse-shape [s]
   (let [id (->> s first str parse-long)
         shape (->> (subs s 3) str/split-lines (mapv vec))]
-    [id shape]))
+    [id
+     (into #{}
+           (for [y (range (count shape))
+                 x (range (count (first shape)))
+                 :when (= \# (get-in shape [y x]))]
+             [y x]))]))
 
 (defn parse-region [s]
   (let [[size & counts] (str/split s #"\s")]
@@ -19,4 +24,54 @@
         regions (->> segments last str/split-lines (map parse-region))]
     [shapes regions]))
 
-(parse-input sample)
+(defn draw-shape [points]
+  (let [ys (map first points)
+        xs (map second points)
+        min-y (reduce min ys)
+        max-y (reduce max ys)
+        min-x (reduce min xs)
+        max-x (reduce max xs)]
+    (str/join
+     "\n"
+     (for [y (range min-y (inc max-y))]
+       (str/join
+        (for [x (range min-x (inc max-x))]
+          (if (contains? points [y x]) \# \.)))))))
+
+(defn rotate [shape]
+  (let [theta (Math/toRadians 90)]
+    (into #{}
+          (comp
+           (map #(map dec %))
+           (map (fn [[y x]]
+                  [(+ (* x (Math/sin theta))
+                      (* y (Math/cos theta)))
+
+                   (- (* x (Math/cos theta))
+                      (* y (Math/sin theta)))]))
+           (map #(mapv Math/round %))
+           (map #(map inc %)))
+          shape)))
+
+(defn reflect [shape]
+  (let [theta (Math/toRadians 180)]
+    (into #{}
+          (comp
+           (map #(map dec %))
+           (map (fn [[y x]]
+                  [(- (* x (Math/sin theta))
+                      (* y (Math/cos theta)))
+
+                   (+ (* x (Math/cos theta))
+                      (* y (Math/sin theta)))]))
+           (map #(mapv Math/round %))
+           (map #(map inc %)))
+          shape)))
+
+(let [shape (get-in (parse-input sample) [0 0])]
+  (println (draw-shape shape))
+  (println)
+  (println (draw-shape (reflect shape)))
+  (println)
+  (println (draw-shape (rotate (rotate shape))))
+  (println))
